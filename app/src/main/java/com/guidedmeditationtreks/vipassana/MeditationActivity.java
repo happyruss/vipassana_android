5746e0fc3e04a98155aa820e935cadc797cc60b5
Russell Dobda <russ.dobda@synervoz.com>
2018-02-11 15:40:32 -0700

    tweaks

--- app/src/main/java/com/guidedmeditationtreks/vipassana/MeditationActivity.java
+++ app/src/main/java/com/guidedmeditationtreks/vipassana/MeditationActivity.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
